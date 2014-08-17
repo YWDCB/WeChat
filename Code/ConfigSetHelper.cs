ac0a439f6426be489189fcd0ae444aa5b8ce3bb7
YWDCB <ydb1990@foxmail.com>
2014-08-17 22:29:12 +0800

    引用DLL
    引用Dll  添加类

---
+++ Code/ConfigSetHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace WeChat.Code
+{
+    /// <summary>
+    /// 配置帮助类
+    /// </summary>
+    public class ConfigSetHelper
+    {
+        /// <summary>
+        /// 调用接口凭证
+        /// </summary>
+        public static string Token = ConfigurationManager.AppSettings["Token"];
+    }
+}
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Senparc.Weixin.MP;
+using WeChat.Code;
 
 namespace WeChat
 {
     public partial class Default : System.Web.UI.Page
     {
+        public readonly string Token = ConfigSetHelper.Token;
         protected void Page_Load(object sender, EventArgs e)
         {
+            var _signature = Request["signature"];
+            var _timestamp = Request["timestamp"];
+            var _nonce = Request["nonce"];
+            var _echostr = Request["echostr"];
+            if (Request.HttpMethod == "GET")
+            {
+                if (CheckSignature.Check(_signature, _timestamp, _nonce, Token))
+                {
+                    WirteContent(_echostr);
+                }
+                else
+                {
+                    WirteContent("failed:" + _signature + "," + CheckSignature.GetSignature(_timestamp, _nonce, Token));
+                }
+            }
 
+        }
+        void WirteContent(string str)
+        {
+            Response.Output.Write(str);
         }
     }
 }
